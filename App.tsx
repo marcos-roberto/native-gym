afa65789d0aad6a80f75fc68aa079b5e1aecd84e
Marcos Roberto <marcosroberto@MacBook-Air-de-Marcos.local>
2023-11-22 15:55:58 -0300

    :tada: start project with native base

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { NativeBaseProvider } from "native-base";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <View style={styles.container}>
+          <Text style={{ fontFamily: "Roboto_700Bold" }}>
+            Open up App.sx to stok art working on your app! macbook air
+          </Text>
+        </View>
+      ) : (
+        <Text>Font is not loaded</Text>
+      )}
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
