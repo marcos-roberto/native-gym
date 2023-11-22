e78cc1fe622754db682ea95f3fa568a3b79aa504
Marcos Roberto <marcos-roberto@unicodelabs.dev>
2023-11-22 17:00:26 -0300

    :lipstick: add tokens styles to native-base

--- App.tsx
+++ App.tsx
@@ -6,11 +6,13 @@ import {
 } from "@expo-google-fonts/roboto";
 import { NativeBaseProvider } from "native-base";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { THEME } from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
